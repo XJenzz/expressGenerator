e9aa0f87317d672837ea7d06a13cc0270bdabd4a
SepurLegend <161539066+SepurLegend@users.noreply.github.com>
2024-10-22 10:06:04 +0700

    Memperbarui kesalahan ketik

--- app_api/routes/fakultas.js
+++ app_api/routes/fakultas.js
@@ -5,6 +5,7 @@ const fakultasController = require("../controllers/fakultasController");
 router.get("/", fakultasController.getAllFakultas);
 router.post("/", fakultasController.createFakultas);
 router.get("/:id", fakultasController.getFakultasById);
+router.get("/:id", fakultasController.updateFakultas);
 router.put("/:id", fakultasController.deleteFakultas);
 
 module.exports = router;
