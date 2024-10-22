282d4c8d1206abdd134672d629e81d97831f235c
SepurLegend <161539066+SepurLegend@users.noreply.github.com>
2024-10-22 10:33:54 +0700

    menambahkan password di mongodb db

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         await mongoose.connect(
-            "mongodb+srv://yulianusjd:<db_password>@cluster0.ionwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb+srv://yulianusjd:BelajarMongo18@cluster0.ionwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
         );
         console.log("MongoDB Connected...");
     }catch(error){
