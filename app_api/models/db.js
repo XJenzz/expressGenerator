8c501a4139a25aab4eaa606fac0898b1ae18a0d9
Bryan Penuntun <cyberjrs.p@gmail.com>
2024-11-28 19:50:35 +0700

    update

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
   try {
     // Menghubungkan ke MongoDB menggunakan URI koneksi
     await mongoose.connect(
-      "mongodb://bryanp:bryanp@cluster0-shard-00-00.phlqa.mongodb.net:27017,cluster0-shard-00-01.phlqa.mongodb.net:27017,cluster0-shard-00-02.phlqa.mongodb.net:27017/mdpsi5a?ssl=true&replicaSet=atlas-hwgv4g-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb://bryanp:bryanp@cluster0-shard-00-00.phlqa.mongodb.net:27017,cluster0-shard-00-01.phlqa.mongodb.net:27017,cluster0-shard-00-02.phlqa.mongodb.net:27017/mdpdb?ssl=true&replicaSet=atlas-hwgv4g-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
       // "mongodb+srv://nurrachmat:BelajarMongo2024@cluster0.cltawz3.mongodb.net/mdpsi5a?retryWrites=true&w=majority&appName=Cluster0"
     );
     // Jika koneksi berhasil, log pesan ke konsol
